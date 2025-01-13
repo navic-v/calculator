d3b9b20ab965f334848517f2a21b6f35e5df8c43
Navic V. <navic.vo@gmail.com>
2025-01-13 20:36:01 +0700

    Add input variable to store data input
    Clear button will reset input variable to empty
    
    When user click the operation, the number will be stored

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ const screeningOutput = document.querySelector('.output');
 calc.addEventListener('click', (event) => {
     event.preventDefault();
 
+    // Add variable to store the number input
+    let input = [];
+
     let target = event.target;
     if (target.tagName == "BUTTON") {
+        if (target.className == "operation") {
+            input.push(screeningInput.innerText);
+            console.log(input);
+        }
         screeningInput.innerText += `${target.innerText} `;
     }
     if (target.id === "clear") {
+        input = [];
         screeningInput.innerText = '';
         screeningOutput.innerText = 0;
     }
