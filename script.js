ec2242e56993b5af7b3c8e34113ed05d038ef039
Navic V. <navic.vo@gmail.com>
2025-01-13 20:36:01 +0700

    Add click event for buttons to show up value on screen

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const container = document.querySelector('container');
+const calc = document.querySelector('.calculator');
+const screeningInput = document.querySelector('.input');
+const screeningOutput = document.querySelector('.output');
+
+calc.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let target = event.target;
+    if (target.tagName == "BUTTON") {
+        screeningInput.innerText += `${target.innerText} `;
+    }
+    if (target.id === "clear") {
+        screeningInput.innerText = '';
+    }
+})
